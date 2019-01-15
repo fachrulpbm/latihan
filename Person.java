b58e2edd36c8ff8bc77fa6847b6eb5a04c0c1299
Fachrul Pralienka Bani Muhamad <fachrulpbm@Fachruls-MacBook-Pro.local>
2019-01-15 18:23:49 +0700

    Person id

--- Person.java
+++ Person.java
@@ -1,17 +1,23 @@
 public class Person{
 
+    private String id;
     private String name;
     
-    public Person(String name){
+    public Person(String id, String name){
+        this.id = id;
         this.name = name;        
+    }
+
+    public String getId(){
+        return id;
     }
     
     public String getName(){
         return name;
     }
     public static void main(String[] args) {
-        Person p = new Person("Joko");
-        System.out.println(p.getName());
+        Person p = new Person("P001","Joko");
+        System.out.println(p.getId()+" "+p.getName());
     }
 
 }
